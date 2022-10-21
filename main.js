21ee39b4b7fc799707f020238481a46225d45d9e
Dalitso <dalitbanda@gmail.com>
2022-10-20 20:01:46 -0400

    parse JSON data

---
+++ main.js
@@ -0,0 +1,24 @@
+
+import fetch from "node-fetch"
+const zipcode = "10026"
+const url = `https://ctp-zip-api.herokuapp.com/zip/${zipcode}`
+//import fetch from "node-fetc
+const PERSON_API_KEY = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+//const url = `https://api.themoviedb.org/3/person/popular?api_key=${PERSON_API_KEY}&language=en-US&page=1`
+// fetch(url)
+//     .then(res => { res.json() })
+//     .then(data => { console.log(data) })
+//     .catch(err => { console.log(`error ${err}`) })
+
+
+
+    console.log(locations)
+
+
+    //let objects  = data.map(city => {c:city. })
+
+}
+
+
+fetchData(url)
+
--- src/App.js
+++ src/App.js
@@ -1,12 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import fetch from "node-fetch"
+
+const zipcode = "10026"
+
+
+async function fetchData(url, zipcode) {
+  const url = `https://ctp-zip-api.herokuapp.com/zip/${zipcode}`
+  let response = await fetch(url);
+  let data = await response.json();
+
+  return data.map((element) => {
+    return {
+      city: element.LocationText,
+      state: element.State,
+      location: [element.Lat, element.Lat],
+      population: element.EstimatedPopulation,
+      totalWages: element.TotalWages,
+
+    }
+  });
+}
 
 function City(props) {
-  return <div>This is the City component</div>;
+  const { location, state, population, totalWages } = [...props]
+  return (
+    <div>
+      <h1>{location}</h1>
+      <ul>
+        <li>{`State: ${state}`}</li>
+        <li>{`location: ${location}`}</li>
+        <li>{`Population (estimate): ${population}`}</li>
+        <li>{`Total Wages: ${totalWages}`}</li>
+      </ul>
+    </div>
+  )
 }
 
 function ZipSearchField(props) {
-  return <div>This is the ZipSearchField component</div>;
+  const [zip, setInputZip] = useState('0000')
+  const handleChange = (event) => {
+    event.persist();
+    
+
+    (event.target.value.length >= 5) ? setInputZip((zip) => ({
+      zip: event.target.value
+    })) : zip
+  }
+
+
+  return (
+    <>
+      <form>
+        <label>
+          Zip-Code
+          <input
+            id="zip-search-field"
+            class="search-field"
+            type="text"
+            placeholder="zip"
+            name="zip-code"
+            value={zip}
+            oncChange={handleChange}
+          />
+        </label>
+      </form>
+    </>
+  )
+}
+
+function Results(props) {
+  return (
+    <>
+    </>)
 }
 
 function App() {
@@ -18,12 +84,11 @@ function App() {
       <div className="mx-auto" style={{ maxWidth: 400 }}>
         <ZipSearchField />
         <div>
-          <City />
-          <City />
+          <Results />
         </div>
       </div>
     </div>
   );
 }
 
-export default App;
+
